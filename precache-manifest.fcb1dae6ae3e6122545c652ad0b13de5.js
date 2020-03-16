f3b95af8a48ab9164db1cdfdb8a3882e7c5b54fb
longdara <35132906+longdara@users.noreply.github.com>
2020-03-16 11:51:18 -0700

    Updates

---
+++ precache-manifest.fcb1dae6ae3e6122545c652ad0b13de5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06718665b4aac3bcd7ffee5da39416c3",
+    "url": "/getweather/index.html"
+  },
+  {
+    "revision": "a6f1151a865e40888e59",
+    "url": "/getweather/static/css/2.52e2994d.chunk.css"
+  },
+  {
+    "revision": "fad40c144aa25e0791e7",
+    "url": "/getweather/static/css/main.a31bfd26.chunk.css"
+  },
+  {
+    "revision": "a6f1151a865e40888e59",
+    "url": "/getweather/static/js/2.40c15f6d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/getweather/static/js/2.40c15f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad40c144aa25e0791e7",
+    "url": "/getweather/static/js/main.bb0c8ac2.chunk.js"
+  },
+  {
+    "revision": "b4b68270ce6d6a32c3b6",
+    "url": "/getweather/static/js/runtime-main.079be0c7.js"
+  },
+  {
+    "revision": "aba485e8b469cc688e97e05285caaa46",
+    "url": "/getweather/static/media/12.aba485e8.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/getweather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/getweather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/getweather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/getweather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/getweather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
